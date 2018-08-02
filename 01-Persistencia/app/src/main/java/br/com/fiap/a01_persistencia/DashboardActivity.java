234da9ba302c3fc13f14cba89715fa1beb737b0d
Leandro Romano <webleandroromano@gmail.com>
2018-08-02 20:47:56 -0300

    Continuação da Aula 01 com persistencia em arquivo de texto.

--- 01-Persistencia/app/src/main/java/br/com/fiap/a01_persistencia/DashboardActivity.java
+++ 01-Persistencia/app/src/main/java/br/com/fiap/a01_persistencia/DashboardActivity.java
@@ -4,8 +4,18 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    EditText edtInfo;
 
     SharedPreferences sp;
 
@@ -13,6 +23,8 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        edtInfo = findViewById(R.id.edtInfo);
 
         sp = getSharedPreferences("login", MODE_PRIVATE);
     }
@@ -24,5 +36,27 @@ public class DashboardActivity extends AppCompatActivity {
         edit.remove("conectado");
         edit.commit();
         finish();
+    }
+
+    public void lerArquivo(View view) {
+        try {
+            FileInputStream fis = openFileInput("bd.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String txt = br.readLine();
+            edtInfo.setText(txt);
+        } catch (java.io.IOException e) {
+            Toast.makeText(this, R.string.msg_erro_recuperar_arquivo, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void salvarArquivo(View view) {
+        try {
+            FileOutputStream fos = openFileOutput("bd.txt", MODE_PRIVATE);
+            String txt = edtInfo.getText().toString();
+            fos.write(txt.getBytes());
+            fos.close();
+        } catch (java.io.IOException e) {
+            Toast.makeText(this, R.string.msg_erro_gravar_arquivo, Toast.LENGTH_SHORT).show();
+        }
     }
 }
